2debb19feba1bc950473f88baee4a4980e0c0392
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-02-16 21:47:31 +0600

    Added Active class to the Vocabulary section

--- script/index.js
+++ script/index.js
@@ -2,14 +2,25 @@ const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all")
         .then(res => res.json())
         .then(data => displayLessons(data.data))
+};
+
+
+const removeActive = () => {
+    const lessonButtons = document.querySelectorAll(".lesson-btn");
+    lessonButtons.forEach(btn => btn.classList.remove("active"));
 }
 
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayLevelWord(data.data));
-}
+        .then(data => {
+            removeActive();
+            const clickBtn = document.getElementById(`lesson-btn-${id}`);
+            clickBtn.classList.add("active")
+            displayLevelWord(data.data);
+        });
+};
 
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container");
@@ -30,10 +41,12 @@ const displayLevelWord = (words) => {
     words.forEach(word => {
         const card = document.createElement("div");
         card.innerHTML = `
-            <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4 h-full">
-            <h2 class="text-2xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
-            <p class="font-semibold">Meaning / Pronunciation</p>
-            <p class="text-2xl font-medium font-bangla">${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি "}</p>
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4 h-full">
+            <div class="">
+                <h2 class="text-2xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
+                <p class="font-semibold">Meaning / Pronunciation</p>
+                <p class="text-2xl font-medium font-bangla">${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি "}</p>
+            </div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume"></i></button>
@@ -42,21 +55,21 @@ const displayLevelWord = (words) => {
         `;
         wordContainer.append(card);
     });
-}
+};
 
 const displayLessons = (lessons) => {
     const levelContainer = document.getElementById("level-container");
     levelContainer.innerHTML = "";
     for (let lesson of lessons) {
         const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `<button onClick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+        btnDiv.innerHTML = `<button id="lesson-btn-${lesson.level_no}" onClick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
         <i class="fa-solid fa-circle-question"></i>Lesson - ${lesson.level_no}
         </button>`
 
         levelContainer.append(btnDiv)
     }
-}
+};
 
 
 
-loadLessons()
+loadLessons();
